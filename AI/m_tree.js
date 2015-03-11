d3e124002934066f456334e463f584171cf3ac71
gblackout <wblackout@163.com>
2015-03-11 16:45:28 +0800

    parameter objects

--- AI/m_tree.js
+++ AI/m_tree.js
@@ -1,16 +1,15 @@
 /**
  * Created by 缘 on 2015/3/7.
  */
+
+/** import modules */
 var bt = require('./BehaviorTree.js-master/btree-complete.min.js');
+var kdtree = require('./kdTree-min.js');
 
 /** coordinates object */
 var Coor = function(x,y){
     this.x = x;
     this.y = y;
-};
-
-Coor.prototype.dist = function(coor){
-    return Math.sqrt(Math.pow(this.x-coor.x,2) + Math.pow(this.y-coor.y,2));
 };
 
 /** speed object, it is a vector which indicate the players current speed on x axis and y axis */
@@ -20,7 +19,7 @@ var Speed = function(x,y){
 };
 
 /** Para object */
-var Para = function(p_coors, p_speeds, o_coors, i_speed, i_coor, p_range){
+var Para = function(p_coors, p_speeds, o_coors, i_speed, i_coor, p_range, i_range){
     // p_coors and o_coors are array of coor that indicate the object IN SIGHT OF the AI
 
     /** fields that you need to input for AI next movement */
@@ -30,12 +29,40 @@ var Para = function(p_coors, p_speeds, o_coors, i_speed, i_coor, p_range){
     this.i_speed = i_speed;
     this.i_coor = i_coor;
     this.p_range = p_range;
+    this.i_range = i_range;
 
     /** fields that AI trees will assign to*/
     this.i_next_coor = this.i_coor;
-    // index indicates the stuff been destroyed by AI in the array o_coors, -1 indicates none is destroyed
-    this.stuff_index = -1;
+    // index indicates the stuff been destroyed by AI in the array o_coors, [undefined,undefined] indicates none is destroyed
+    this.stuff = [undefined,undefined];
 };
+
+/** Euclidean distance*/
+var distance = function(a, b){
+    return Math.sqrt(Math.pow(a.x - b.x, 2) +  Math.pow(a.y - b.y, 2));
+};
+
+/** return the nearest available object to be destroyed: [index,dist_to_ai], return [undefined,undefined] if not exists*/
+function get_near_aval(para){
+
+    var points = new Array(para.p_coors.length);
+
+    for(var i=0;i<para.p_coors.length;i++){
+        points[i] = {x:para.p_coors[i][0],y:para.p_coors[i][1]};
+    }
+    var tree = new kdtree.kdTree(points, distance, ["x", "y"]);
+
+    var cand=[undefined, undefined];
+    for(var j=0;j<para.o_coors.length;j++){
+        var nearest = tree.nearest({ x: para.o_coors[i][0], y: para.o_coors[i][1] },1,para.p_range);
+        if( nearest[0] !== undefined && (cand[0] === undefined || cand[1] > nearest[0][1])){
+            cand[0] = j;
+            cand[1] = distance({x:para.i_coor[0],y:para.i_coor[1]},{x:para.o_coors[j][0],y:para.o_coors[j][1]});
+        }
+    }
+
+    return cand;
+}
 
 var btree = new bt({
     title:'AI behavior',
@@ -50,22 +77,17 @@ var btree = new bt({
                         nodes: [
                             new bt.Task({
                                 title: 'con:search_if_has',
-                                run : function(){
-                                    //TODO
-                                    if(1){
-                                        console.log('con:search_if_has');
-                                        this.success();
-                                    }else{
+                                run : function(para){
+                                    var cand = get_near_aval(para);
+                                    if(cand[0] === undefined){
                                         console.log('con:search_if_has_fail');
                                         this.fail();
+                                    }else{
+                                        console.log('con:search_if_has');
+                                        console.log('return_closet');
+                                        para.stuff = cand;
+                                        this.success();
                                     }
-                                }
-                            }),new bt.Task({
-                               title: 'return_closet',
-                                run : function(para){
-                                    //TODO
-                                    console.log('return_closet');
-                                    this.success();
                                 }
                             })
                         ]
@@ -77,9 +99,8 @@ var btree = new bt({
                                 nodes: [
                                     new bt.Task({
                                         title: 'con:is_close',
-                                        run: function(){
-                                            //TODO
-                                            if(0){
+                                        run: function(para){
+                                            if(para.i_range >= para.stuff[1]){
                                                 console.log('con:is_close');
                                                 this.success();
                                             }else{
@@ -89,8 +110,8 @@ var btree = new bt({
                                         }
                                     }), new bt.Task({
                                         title: 'kill',
-                                        run : function(){
-                                            //TODO
+                                        run : function(para){
+                                            // do nothing
                                             console.log('kill');
                                             this.success();
                                         }
@@ -98,9 +119,12 @@ var btree = new bt({
                                 ]
                             }),new bt.Task({
                                 title: 'get_closer',
-                                run: function(){
-                                    // TODO
+                                run: function(para){
                                     console.log('get_closer');
+
+                                    para.i_next_coor[0] = para.i_coor[0] + para.i_speed * (para.stuff[0][0] - para.i_coor[0]) / para.stuff[1];
+                                    para.i_next_coor[1] = para.i_coor[1] + para.i_speed * (para.stuff[0][1] - para.i_coor[1]) / para.stuff[1];
+                                    para.stuff = [undefined,undefined];
                                     this.success();
                                 }
                             })
